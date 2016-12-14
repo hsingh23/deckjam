8c42c7f4186e88e8701bc57a511ac2c3da25dbbe
Harsh Singh <harsingh@cimpress.com>
2016-12-13 21:55:18 -0500

    Load smaller chunks of quizlet decks, better mobile experience

--- app.js
+++ app.js
@@ -34,20 +34,25 @@ var app = angular.module('deckjam', ['ngMaterial', 'angulartics', 'angulartics.g
       icon: '@',
       style: '@?'
     },
-    template: '<md-tooltip style="{{style}}" hide-gt-xs="hide-gt-xs">{{tip}}</md-tooltip><md-icon hide-gt-xs="hide-gt-xs" class="material-icons" style="{{style}}">{{icon}}</md-icon><span style="{{style}}" hide-xs>{{tip}}</span>'
+    template: `<md-tooltip style="{{style}}" hide-gt-xs="hide-gt-xs">
+      {{tip}}
+    </md-tooltip>
+    <md-icon hide-gt-xs="hide-gt-xs" class="material-icons" style="{{style}}">
+      {{icon}}
+    </md-icon>
+    <span style="{{style}}" hide-xs>{{tip}}</span>`
   }
 })
 .controller('homeContainer', ["$scope", "$http", "$mdToast", "$mdMedia" ,(_, $http, $mdToast, $mdMedia)=> {
   _.api = 'http://ayudh.org:3337'
-  _._sm = !$mdMedia('sm')
-  _.sm = $mdMedia('sm')
+  _._sm = ()=> !$mdMedia('md')
+  _.sm = ()=> $mdMedia('md')
   // _.api = 'http://localhost:3337'
   _.createdOne = localStorage.createdOne && parseInt(localStorage.createdOne) || 0
   _.fetching = false
   _.getSetsforTerm = (term)=> $http.get(`${_.api}/quizlet/search?query=${term}`, { cache: true})
   _.getSets = (sets)=> $http.get(`${_.api}/quizlet/sets?query=${sets}`, { cache: true})
-  _.decks = {}
-  // _.decks = JSON.parse(localStorage.decks || '{}')
+  _.decks = JSON.parse(localStorage.decks || '{}')
   _.selected = JSON.parse(localStorage.selected || '{}')
   _.selectedOrder = "time"
   _.reverse = true
@@ -55,8 +60,7 @@ var app = angular.module('deckjam', ['ngMaterial', 'angulartics', 'angulartics.g
   _.numSelected = ()=> lo.size(_.selected)
   _.numDecks = ()=> lo.size(_.decks)
   _.selectedArray = ()=> lo.values(_.selected)
-  _.selectTerm = (term, setId, mouseDown=true) => {
-    term.selected = (!!mouseDown) ? !!!term.selected : !!term.selected
+  function selectTerm(term, setId) {
     if (term.selected) {
       _.selected[term.id] = lo.assign({},term)
       _.selected[term.id].setId = setId
@@ -65,6 +69,20 @@ var app = angular.module('deckjam', ['ngMaterial', 'angulartics', 'angulartics.g
       delete _.selected[term.id]
     }
     localStorage.selected = JSON.stringify(_.selected)
+  }
+  _.selectClickTerm = (term, setId) => {
+    // checkbox check in small, whole rows checks >sm
+    if(!$mdMedia('md')) {
+      term.selected = !term.selected
+      selectTerm(term, setId)
+    }
+  }
+  _.selectDragTerm = (term, setId, mouseDown=true) => {
+    // checkbox check in small, whole rows checks >sm
+    if($mdMedia('md')){
+      term.selected = (mouseDown) ? !term.selected : term.selected
+      selectTerm(term, setId)
+    }
   }
   _.removeDeck = id=> delete _.decks[id]
   _.selectAll = id=> {
@@ -126,9 +144,11 @@ var app = angular.module('deckjam', ['ngMaterial', 'angulartics', 'angulartics.g
         $mdToast.showSimple("Your deck is created")
         _.createdOne += 1
         localStorage.createdOne = _.createdOne
+        _.selected_actions = 'home'
       }
     }).catch(()=>{
       _.creating=false
+      _.selected_actions = 'home'
       $mdToast.showSimple("Unable to create deck")
     })
   }
@@ -137,6 +157,7 @@ var app = angular.module('deckjam', ['ngMaterial', 'angulartics', 'angulartics.g
     var x = importUrl && importUrl.match(/\d+/)
     x && x[0] && _.getSets(x[0]).then(res=>{
       _.fetching = false
+      _.selected_actions = 'home'
       res.data.forEach(set=> {
         set.terms.forEach(term => {
           term.selected = true
@@ -145,44 +166,52 @@ var app = angular.module('deckjam', ['ngMaterial', 'angulartics', 'angulartics.g
           _.selected[term.id].time = (new Date()).getTime()
         })
       })
-    }).catch(()=>{_.fetching = false})
+    }).catch(()=>{
+      _.fetching = false
+      _.selected_actions = 'home'
+    })
     localStorage.selected = JSON.stringify(_.selected)
+  }
+  function getSets(setIds) {
+    _.getSets(setIds.map(a=>a.id).join(','))
+    .then(res => {
+      res.data.forEach(set=> {
+        var terms = lo.filter(set.terms, card=> {
+          if (_.bloom.test(card.term + card.definition)) {
+            return false
+          } else {
+            _.bloom.add(card.term + card.definition)
+            return true
+          }
+        })
+        terms.forEach((t,i) => t.rank = i)
+        if (terms.length > 2){
+          _.decks[set.id] = lo.pick(set, ['url', 'title', 'modified_date', 'lang_terms', 'lang_definitions'])
+          _.decks[set.id].terms = terms
+          _.decks[set.id].terms_length = terms.length
+        }
+      })
+      _.fetching = false
+      _.selectedIndex=0
+      localStorage.decks = JSON.stringify(_.decks)
+    }).catch(()=>{
+      _.fetching = false
+      $mdToast.showSimple("Unable to get terms - try another query")
+    })
   }
   _.getTerms = (replace)=> {
     _.fetching = true
     _.decks = !!replace ? {} : (_.decks || {})
     _.bloom = !!replace ? new BloomFilter(3e5, 3e-5) : (_.bloom || new BloomFilter(3e5, 3e-5))
     _.search.split(',').forEach(term=> {
-      _.getSetsforTerm(term.trim()).then(data=> {
-        _.getSets(data.data.sets.slice(0,20).map(a=>a.id).join(','))
-          .then(res => {
-            res.data.forEach(set=> {
-              var terms = lo.filter(set.terms, card=> {
-                if (_.bloom.test(card.term + card.definition)) {
-                  return false
-                } else {
-                  _.bloom.add(card.term + card.definition)
-                  return true
-                }
-              })
-              terms.forEach((t,i) => t.rank = i)
-              if (terms.length > 2){
-                _.decks[set.id] = lo.pick(set, ['url', 'title', 'modified_date', 'lang_terms', 'lang_definitions'])
-                _.decks[set.id].terms = terms
-                _.decks[set.id].terms_length = terms.length
-              }
-            })
-            _.fetching = false
-            _.selectedIndex=0
-            // localStorage.decks = JSON.stringify(_.decks)
-          }).catch(()=>{
-            _.fetching = false
-            $mdToast.showSimple("Unable to get terms - try another query")
-          })
-        }).catch(()=>{
-          _.fetching = false
-          $mdToast.showSimple("Unable to get terms - try another query")
-        })
+      _.getSetsforTerm(term.trim()).then(res=> {
+        getSets(res.data.sets.slice(0,10))
+        getSets(res.data.sets.slice(10,20))
       })
-    }
+      .catch(()=>{
+        _.fetching = false
+        $mdToast.showSimple("Unable to get terms - try another query")
+      })
+    })
+  }
 }])
